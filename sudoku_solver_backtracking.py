6ef8fe79fa07d1963e82e3e0a6bbe7a9a2a24a2b
SantiagoIvan <48731203+SantiagoIvan@users.noreply.github.com>
2022-04-19 17:37:21 -0300

    Update sudoku_solver_backtracking.py
    solve nxn sudokus, siendo n natural y sqrt(n) natural

--- sudoku_solver_backtracking.py
+++ sudoku_solver_backtracking.py
@@ -1,3 +1,5 @@
+import math 
+
 puzzle = [
     [5, 3, 0, 0, 7, 0, 0, 0, 0],
     [6, 0, 0, 1, 9, 5, 0, 0, 0],
@@ -97,41 +99,42 @@ def is_in_the_row(puzzle, row, n):
 
 
 def is_in_the_column(puzzle, column, n):
-    return n in [puzzle[i][column] for i in range(9)]
+    return n in [puzzle[i][column] for i in range(len(puzzle))]
 
 
 def is_in_the_region(puzzle, row, column, n):
-    row_start_index = row - row % 3
-    column_start_index = column - column % 3
-    for i in range(row_start_index, row_start_index + 3):
-        for j in range(column_start_index, column_start_index + 3):
+    sqrt = int(math.sqrt(len(puzzle)))
+    row_start_index = row - row % sqrt
+    column_start_index = column - column % sqrt
+    for i in range(row_start_index, row_start_index + sqrt):
+        for j in range(column_start_index, column_start_index + sqrt):
             if puzzle[i][j] == n:
                 return True
     return False
 
 
 def search_free_square(puzzle):
-    for i in range(9):
-        for j in range(9):
+    dim = len(puzzle)
+    for i in range(dim):
+        for j in range(dim):
             if not puzzle[i][j]:
                 return (i, j)
     return None
 
 
 def is_completed(puzzle):
-    for i in range(9):
-        for j in range(9):
+    for i in range(len(puzzle)):
+        for j in range(len(puzzle)):
             if not puzzle[i][j]:
                 return False
     return True
 
-
-def sudoku(puzzle):
+def solve(puzzle):
 
     coords = search_free_square(puzzle)
     if coords:
         (row, column) = coords
-        for n in range(1, 10):
+        for n in range(1, len(puzzle)+1):
             if (
                 (not is_in_the_row(puzzle, row, n))
                 and (not is_in_the_column(puzzle, column, n))
@@ -139,19 +142,20 @@ def sudoku(puzzle):
             ):
                 # si el numero en cuestion es un numero valido, lo inserto
                 puzzle[row][column] = n
-                res = sudoku(puzzle)
+                res = solve(puzzle)
                 if res:
                     return res
                     # si alguno de mis hijos me retorna un puzzle, significa que lo pude terminar y lo retorno
         # si no sirvio ninguno de estos numeros, pongo un 0 en mi lugar asi lo vacio,
         # retorno a quien me llamo un false para que luego, la funcion search_free_square pueda volver a encontrar estos bloques libres
         puzzle[row][column] = 0
-        return False
+        return None
+    
     return puzzle
 
 
 # Test
-print(sudoku(puzzle) == solution)
-print(sudoku(test1) == solution1)
-print(sudoku(test2) == solution2)
-print(sudoku(test3) == solution3)
+print(solve(puzzle) == solution)
+print(solve(test1) == solution1)
+print(solve(test2) == solution2)
+print(solve(test3) == solution3)
